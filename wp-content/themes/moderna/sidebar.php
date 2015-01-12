165db7c06e3c35ed2228526b85237282245c09d4
pelicanpaul <thepaullyons@gmail>
2015-01-12 13:20:06 -0800

    removed moderna theme

--- wp-content/themes/moderna/sidebar.php
+++
@@ -1,8 +0,0 @@
-<div class="col-lg-4">   
-	<aside class="<?php echo iwebtheme_smof_data('sidebar_pos'); ?>-sidebar">
-    <?php 
-		/* Primary Sidebar */
-		dynamic_sidebar('Primary Sidebar');
-	?>	
-	</aside>
-</div>
